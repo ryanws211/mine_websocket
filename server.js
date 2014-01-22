156e0efb90bdf88841c1128e465e54904be0e0f2
Charles Burton <pi@raspberrypi.(none)>
2014-01-22 09:17:25 -0700

    Started work on parsing JSON to split it to different hardware functions

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var WebSocketServer = require('ws').Server;
+var logger = require('caterpillar').createLogger({6:6});
 var wss = new WebSocketServer({port: (process.env.PORT || 5000)});
+var pif= require('caf_piface');
+var pfio = new pif.PiFace();
+pfio.init();
+pfio.write(1,1);
+pfio.write(0,1);
 var webSockets = {}; //userID: webSocket
+
+//Setup Logging
+logger.pipe(require('fs').createWriteStream('/tmp/server.log'));
+logger.log("Logging Started");
 
 //Connect /:userID
 var userID = 0;
@@ -11,13 +21,17 @@ wss.on('connection', function (webSocket) {
     userID = ++userID;
     
     webSocket.on('message', function(message) {
-        //var messageArray = JSON.parse(message);
-        console.log(message);
-        multiSend(message);
+        try {
+            var json = JSON.parse(message);
+            parseMessage(json);
+            multiSend("JSON: " + message);
+        } catch (e) {
+            multiSend("Not JSON:" + message);
+        }
     });
 
     webSocket.on('close', function() {
-        console.log("Connection Closed");
+        logger.log("Connection Closed");
     });
 
 
@@ -34,4 +48,25 @@ wss.on('connection', function (webSocket) {
     }
 
 
+    function parseMessage(message) {
+        if (message.read) {
+            console.log(message.read);
+        } else if (message.write) {
+            console.log(message.write['1']);
+            writePin(message.write);
+        } else if (message.log) {
+            console.log(message.log);
+        } else if (message.broadcast) {
+            console.log(message.broadcast);
+        }
+    }
+
+    function writePin(message) {
+        if (message['1']) {
+            pfio.write(1,1);
+            process.nextTick(function() {
+                pfio.write(0,1);
+            });
+        }
+    }
 });
